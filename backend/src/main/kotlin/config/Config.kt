fcbe24585661f95efeb2271407470b3471b605d5
alramalho <alexandre.ramalho.1998@gmail.com>
2021-02-27 19:57:38 +0000

    Docker fixes: - Remove bckend testing when deploying - Expose frontend in webpack

--- backend/src/main/kotlin/config/Config.kt
+++ backend/src/main/kotlin/config/Config.kt
@@ -5,14 +5,17 @@ import database.PostgreRepository
 import org.jetbrains.exposed.sql.Database
 
 object Config {
+    private val DB_HOST = System.getenv("DB_HOST") ?: "localhost"
+    private val DB_PORT = System.getenv("DB_PORT") ?: "5432"
+    private val API_KEY = System.getenv("API_KEY") ?: "76a325g7g2ahs7h4673aa25s47632h5362a4532642"
 
     val repository by lazy {
         PostgreRepository(
-            Database.connect(url = "jdbc:postgresql://${System.getenv("DB_HOST")}:${System.getenv("DB_PORT")}/main", user="admin", password = "admin")
+            Database.connect(url = "jdbc:postgresql://${DB_HOST}:${DB_PORT}/main", user="admin", password = "admin")
         ).also(PostgreRepository::updateSchema)
     }
 
     val storesGateway by lazy {
-        StoresAPIGateway(apiUrl = "http://134.209.29.209", apiKey = System.getenv("API_KEY"))
+        StoresAPIGateway(apiUrl = "http://134.209.29.209", apiKey = API_KEY)
     }
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source set_env.sh $@
+source set_env.sh
 
 _term() {
   echo "🔥  Fire crash burn  🔥"
